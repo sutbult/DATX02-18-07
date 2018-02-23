1e2bb6985332755f6d715c03d6a1d97cd9587394
Samuel Utbult <samuelutbult@Samuels-MacBook-Pro.local>
2018-02-23 13:37:16 +0100

    Add bids types
    Refs #1

---
+++ client/src/Browse/Bids/Types.elm
@@ -0,0 +1,17 @@
+module Browse.Bids.Types exposing (..)
+
+type alias Value =
+    { currency : String
+    , amount : Float
+    }
+
+type alias Bid =
+    { from : Value
+    , to : Value
+    }
+
+type alias Model =
+    { bids : List Bid
+    }
+
+type alias Msg = Never
