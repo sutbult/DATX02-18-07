2b4f7fb3dc7bb0a8b67d6bf85b1204368d599de2
Samuel Utbult <samuelutbult@Samuels-MacBook-Pro.local>
2018-03-09 13:44:46 +0100

    Implement api server

---
+++ src/api/server.js
@@ -0,0 +1,76 @@
+
+const http = require("http");
+const express = require("express");
+
+const api = require("./index.js");
+
+const apiRouter = express.Router();
+
+
+// Body management setup
+
+apiRouter.use((req, res, next) => {
+	var body = [];
+	req.on('data', (chunk) => body.push(chunk));
+	req.on('end', () => {
+		var rawBody = Buffer.concat(body).toString();
+		try {
+			req.body = JSON.parse(rawBody);
+		}
+		catch(e) {
+            req.body = {};
+		}
+        next();
+	});
+});
+
+
+// Endpoints
+
+apiRouter.get("/getBids", (req, res, next) => {
+    api.getBids((error, bids) => {
+        next(bids);
+    });
+});
+apiRouter.post("/addBid", (req, res, next) => {
+    api.addBid(req.body, (error) => {
+        next({});
+    });
+});
+apiRouter.post("/acceptBid", (req, res, next) => {
+    api.acceptBid(req.body, (error) => {
+        next({});
+    });
+});
+
+
+// Server stuff
+
+apiRouter.get("/*", (req, res, next) => {
+	next(404);
+});
+apiRouter.use((prev, req, res, next) => {
+    var response;
+    var code;
+
+    if(typeof prev === "number") {
+        code = prev;
+        response = {};
+    }
+    else {
+        code = 200;
+        response = prev;
+    }
+    res.status(code);
+    res.type("application/json");
+    res.send(JSON.stringify(response));
+    res.end();
+});
+
+const app = express();
+app.use("/api", apiRouter);
+
+const server = http.createServer(app);
+server.listen(51337, "localhost", () => {
+    console.log("Daemon is now running");
+});
