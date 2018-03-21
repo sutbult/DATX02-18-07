5456fae5a8415470db543a6fb10fe39030ce82ad
Jacob Torrång <kopatheonlyone@hotmail.com>
2018-03-21 20:12:28 +0100

    Add function to send a token contract, add comments and clean up some code

--- src/api/ethereum.js
+++ src/api/ethereum.js
@@ -58,52 +58,64 @@ async function isConnected(ethchain){
 var experimental = new Web3('ws://127.0.0.1:7545');
 var classic =  new Web3('ws://127.0.0.1:8545');
 
-
-console.log(experimental.currentProvider);
-console.log(classic.currentProvider);
-
 /**Query for the compiled abi and bytecode */
 var erc20 = JSON.parse(fs.readFileSync('./contracts/ERC20Partial.json', 'utf8'));
-var abi;
-var bytecode;
 var htlc_ether = JSON.parse(fs.readFileSync('./contracts/HTLC.json', 'utf8'));
 var htlc_erc20 = JSON.parse(fs.readFileSync('./contracts/HTLC_ERC20.json', 'utf8'));
-abi = htlc_ether.abi;
-bytecode = '0x' + htlc_ether.code;
 
+async function unlockAccount(ethchain, account_address, account_password, time_in_ms = 10000){
+    var account = await ethchain.eth.personal.unlockAccount(account_address, account_password, time_in_ms);
+    console.log(account);
+}
 
-async function validateEtherContract(ethchain, jsoncontract, contract_address, self_address, value_in_eth, digest = null){
-    var res_cont = await validateContract(ethchain, jsoncontract, contract_address, self_address, digest);
-    var res_val = await validateERC20Value(ethchain, value_in_eth, contract_address);
+/**  This function will validate an Ether HTLC contract
+*  @param {string} digest - The digest of the contract, if there is any (only for second contracts)
+*  @param {string} self_address - The destination of the contract, ie: yourself
+*/
+async function validateEtherContract(ethchain, contract_address, self_address, value_in_eth, digest = null){
+    var res_cont, res_val;
+    
+    res_cont = await validateContract(ethchain, htlc_ether, contract_address, self_address, digest);
+    res_val = await validateERC20Value(ethchain, value_in_eth, contract_address);
     return res_cont && res_val;
 }
 
-async function validateERC20Contract(ethchain, jsoncontract, contract_address, self_address, token_address, value_in_tokens, decimals = 18, digest = null){
-    var res_cont = await validateContract(ethchain, jsoncontract, contract_address, self_address, digest);
-    var res_address = await validateERC20Address(ethchain, jsoncontract.abi, token_address, contract_address);
-    var res_val = await validateERC20Value(ethchain, value_in_tokens, token_address, contract_address, decimals);
+/**  This function will validate a ERC20 HTLC contract
+*  @param {int} decimals - The number of decimals for the token (default is 18)
+*  @param {string} token_address - The address of the token
+*  @param {string} value_in_tokens - The number of tokens, with decimals, so "1" token is 1 * 10^18 with 18 decimals.
+*  @param {string} digest - The digest of the contract, if there is any (only for second contracts)
+*  @param {string} self_address - The destination of the contract, ie: yourself
+*/
+async function validateERC20Contract(ethchain, contract_address, self_address, token_address, value_in_tokens, decimals = 18, digest = null){
+    var res_cont, res_address, res_val;
+    
+    res_cont = await validateContract(ethchain, htlc_erc20, contract_address, self_address, digest);
+    res_address = await validateERC20Address(ethchain, htlc_erc20.abi, token_address, contract_address);
+    res_val = await validateERC20Value(ethchain, value_in_tokens, token_address, contract_address, decimals);
     return res_cont && res_val && res_address;
 }
 
+/** This function validates parts of a HTLC contract, used by other functions
+*/
 async function validateContract(ethchain, jsoncontract, contract_address, self_address, digest = null){
-    var res_code = await validateCode(ethchain, jsoncontract.runtime_bytecode, contract_address);
-    var res_dest = await validateDestination(ethchain, jsoncontract.abi, self_address, contract_address);
-    var res_digest = true;
+    var res_code, res_dest, res_digest;
+    
+    res_code = await validateCode(ethchain, jsoncontract.runtime_bytecode, contract_address);
+    res_dest = await validateDestination(ethchain, jsoncontract.abi, self_address, contract_address);
+    res_digest = true;
     if(digest != null){
         res_digest = await validateDigest(ethchain, contract_abi, digest, contract_address);
     }
     return res_code && res_dest && res_digest;
 }
 
-async function unlockAccount(ethchain, account_address, account_password, time_in_ms = 10000){
-    ethchain.eth.personal.unlockAccount(account_address, account_password, time_in_ms);
-}
-
-
 /** This function will validate the stored byte code against the byte code on a certain address.
  *  Remember that it's the runtime bytecode that needs to be compared, not the compiletime bytecode
 */
 async function validateCode(ethchain, runtime_bytecode, contract_address){
+    var chain_code;
+    
     chain_code = await ethchain.eth.getCode(contract_address);
     return runtime_bytecode == chain_code;
 }
@@ -112,8 +124,10 @@ async function validateCode(ethchain, runtime_bytecode, contract_address){
 * Assuming you are validating, it should be yourself.
 */
 async function validateDestination(ethchain, contract_abi, dest_address, contract_address) {
-    var contract = new ethchain.eth.Contract(contract_abi, contract_address);
-    var contract_dest = await contract.methods.dest().call();
+    var contract, contract_dest;
+    
+    contract = new ethchain.eth.Contract(contract_abi, contract_address);
+    contract_dest = await contract.methods.dest().call();
     return dest_address == contract_dest;
 }
 
@@ -121,8 +135,10 @@ async function validateDestination(ethchain, contract_abi, dest_address, contrac
  *  This is only necessary for one party.
 */
 async function validateDigest(ethchain, contract_abi, digest, contract_address) {
-    var contract = new ethchain.eth.Contract(contract_abi, contract_address);
-    var contract_digest = await contract.methods.digest().call();
+    var contract, contract_digest;
+    
+    contract = new ethchain.eth.Contract(contract_abi, contract_address);
+    contract_digest = await contract.methods.digest().call();
     return digest == contact_digest;
 }
 
@@ -130,8 +146,9 @@ async function validateDigest(ethchain, contract_abi, digest, contract_address) 
  *  Only works on normal HTLC (With Ether)
 */
 async function validateValue(ethchain, value_in_eth, contract_address){
-    var balance = await ethchain.eth.getBalance(contract_address);
-    console.log(balance);
+    var balance;
+    
+    balance = await ethchain.eth.getBalance(contract_address);
     return Web3.utils.toWei(value_in_eth) == balance;
 }
 
@@ -139,17 +156,21 @@ async function validateValue(ethchain, value_in_eth, contract_address){
  *  Only works on ERC20-HTLC!
 */
 async function validateERC20Value(ethchain, value_in_tokens, token_address, contract_address, decimals = 18){
-    var token = new ethchain.eth.Contract(erc20.abi, token_address);
-    var balance = await token.methods.balanceOf(contract_address).call();
-    return value_in_tokens * Math.pow(10, decimals) == balance;
+    var token, balance;
+    
+    token = new ethchain.eth.Contract(erc20.abi, token_address);
+    balance = await token.methods.balanceOf(contract_address).call();
+    return tokensNoDecimals(value_in_tokens, decimals) == balance;
 }
 
 /** This function validates that the contract sends the correct token when claimed.
  *  Only works on ERC20-HTLC!
 */
 async function validateERC20Address(ethchain, contract_abi, token_address, contract_address){
-    var contract = new ethchain.eth.Contract(contract_abi, contract_address);
-    var contract_token = await contract.methods.addressToken().call();
+    var contract, contract_token;
+    
+    contract = new ethchain.eth.Contract(contract_abi, contract_address);
+    contract_token = await contract.methods.addressToken().call();
     return token_address == contract_token;
 }
 
@@ -157,62 +178,110 @@ async function validateERC20Address(ethchain, contract_abi, token_address, contr
  * This modules main function
  * @param {hex} from_adr -  adress the user wishes to send money from
  */
-function prepareAndDeploy(ethchain, from_adr,p_secret, p_digest, p_dest, p_amount){
 
-    var digest, ether, gasEstimate, contractInstance;
+async function sendERC20Contract(ethchain, from_address, secret = null, digest = null, destination, value_in_tokens, token_address){
+    var args, gen_digest;
+    
+    gen_digest = generateDigest(ethchain, secret, digest);
+    args = [gen_digest, destination, token_address];
+    sendContract(ethchain, htlc_erc20, args, from_address, gen_digest, 0);
+}
+
+async function sendEtherContract(ethchain, from_address, secret, digest, destination, value_in_eth){
+    var args, gen_digest;
+    
+    gen_digest = generateDigest(ethchain, secret, digest);
+    args = [gen_digest, destination];
+    sendContract(ethchain, htlc_ether, args, from_address, gen_digest, value_in_eth);
+}
+ 
+async function sendContract(ethchain, jsoncontract, args, from_address, digest, value_in_eth){
+    var ether, gas_estimate, contract, contract_instance;
     /**If you are bid poster the secret will be keccak256-hashed
      * else p_digest will contain the hashed secret
      */
-    if(p_secret != null){
-        digest = ethchain.utils.sha3(p_secret);
-        console.log("Digest, send to tradee");
-        console.log(digest);
-    }else{
-        digest = p_digest;
-    }
 
     /**Need to convert the user inputted amount to Wei */
-    ether = ethchain.utils.toWei(p_amount, "ether");
-    gasEstimate =  4712386;//web3FirstChain.eth.estimateGas({data: bytecode});
-
-      /**Creating an instance of our HTL contract
-     * Currently using the predefined coinbase of the first chain
-     * @todo make "from:" from_adr once message passing works
-     */
+    ether = ethchain.utils.toWei(value_in_eth, "ether");
+    gas_estimate =  4712386;//web3FirstChain.eth.estimateGas({data: bytecode});
      
-     
-    var contract = new ethchain.eth.Contract(abi);
-    var contractInstance = contract.deploy({data: bytecode, arguments: [digest, p_dest]});
-    contractInstance.send({from: from_adr, gasPrice: gasEstimate.toString(), gas: gasEstimate, value: 0})
-      .once('receipt', function (receipt){
+    
+    contract = new ethchain.eth.Contract(jsoncontract.abi);
+    
+    contract_instance = contract.deploy({data: '0x' + jsoncontract.code, arguments: args});
+    console.log(args);
+    console.log(gas_estimate);
+    console.log(from_address);
+    console.log(ether);
+    contract_instance.send({from: from_address, gasPrice: gas_estimate.toString(), gas: gas_estimate, value: 0})
+        .once('receipt', function (receipt){
         /**@todo send this information to other user */
-        console.log("Contract deployed at " + receipt.blockNumber);
+        console.log("Contract deployed in block " + receipt.blockNumber);
+        console.log("Contract deployed at address " + receipt.contractAddress);
         contract.options.address = receipt.contractAddress;
-        subscribeToClaim(contract, receipt.blockNumber)
-      });
+        subscribeToClaim(contract, receipt.blockNumber);
+      });     
+    /**@todo send this information to other user */
+
+}
+
+
+function generateDigest(ethchain, secret, digest){
+    if(secret != null){
+        return ethchain.utils.sha3(secret);
+    }
+    else{
+        return digest;
+    }
+}
+
+async function sendTokensToContract(ethchain, contract_address, token_address, value_in_tokens, decimals = 18){
+    var token, gas_estimate;
+    
+    gas_estimate =  4712386; 
+    token = new ethchain.eth.Contract(erc20.abi, token_address);
+    return token
+        .methods
+        .transfer(contract_address, tokensNoDecimals(value_in_tokens, decimals))
+        .send({from: from_address, gasPrice: gasEstimate.toString(), gas: gasEstimate, value: 0});
+}
+ 
+function tokensNoDecimals(tokens, decimals) {
+    return tokens * Math.pow(10, decimals);
 }
 
 function subscribeToClaim(contract, block){
+    /** @todo fix callback
+    *
+    */
     contract.events.Claim({fromBlock: "latest"}, function(error, event){console.log(event.returnValues._hash);});
 }
 
-async function getPastClaim(ethchain, jsoncontract, contract_address, from_block = 1){
-    var contract = new ethchain.eth.Contract(jsoncontract.abi, contract_address);
-    var events = await contract.getPastEvents('Claim', {
+
+/** Will return the value of the first Claim event it founds.
+*
+*/
+async function getPastClaim(ethchain, contract_address, from_block = 1){
+    var contract, events;
+    
+    contract = new ethchain.eth.Contract(htlc_ether.abi, contract_address);
+    events = await contract.getPastEvents('Claim', {
       fromBlock: from_block,
       toBlock: 'latest'
     });
     return events[0].returnValues._hash;
 }
 
-function claimContract(ethchain, pre_image_hash, from_adr, claim_adr){
+function claimContract(ethchain, pre_image_hash, from_address, claim_address){
+    var contract;
+    
     /**@todo the account claiming the contract should be based on user input */
 
-    var contract = new ethchain.eth.Contract(abi);
-    contract.options.address = claim_adr;
+    contract = new ethchain.eth.Contract(htlc_ether.abi);
+    contract.options.address = claim_address;
     
-    contract.methods.claim(pre_image_hash).send({from: from_adr}).on("receipt", function(receipt){
+    contract.methods.claim(pre_image_hash).send({from: from_address}).on("receipt", function(receipt){
     console.log("DEPLOYED");});
 }
 
-module.exports = {isConnected, subscribeToClaim, unlockAccount, getPastClaim, experimental, geth, htlc_ether, htlc_erc20, validateCode, validateEtherContract, validateERC20Contract, validateContract, validateDestination, validateValue, validateERC20Value, classic, abi, bytecode, claimContract, prepareAndDeploy, validateContract};
+module.exports = {isConnected, subscribeToClaim, unlockAccount, getPastClaim, experimental, geth, htlc_ether, htlc_erc20, validateCode, validateEtherContract, validateERC20Contract, validateContract, validateDestination, validateValue, validateERC20Value, classic, claimContract, sendTokensToContract, sendERC20Contract, sendEtherContract, validateContract};
