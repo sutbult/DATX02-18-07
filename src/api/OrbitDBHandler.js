cb7bb6059f7c68815df67a06707746783f415b63
petrosdebesay <petrosd@student.chalmers.se>
2018-03-21 15:32:40 +0100

    Add messaging functionality

--- src/api/OrbitDBHandler.js
+++ src/api/OrbitDBHandler.js
@@ -1,9 +1,17 @@
 const Ipfs = require('ipfs')
 const OrbitDB = require('orbit-db')
 
+//1st: Bud och egen address.
+//2nd: accept bid, skickar sin egen adress.
+//1st: lägger upp ett kontrakt, skickar digest och konstraktsadress.
+//2nd: skickar konstraktsadress
+
+
 var globaldb
 var messagedb
 let orbitdb
+var db
+var channels = []
 
 const access = {
    // Give write access to ourselves
@@ -39,8 +47,106 @@ ipfs.once('ready', async function() {
 
 });
 
+async function createChannel(channelName) {
+  return await orbitdb.feed(channelName);
+}
+
+/*
+Bid should be JSON in form of jsonObject = {
+    "step" : "1",
+    "from" : "CURRENCY",
+    "fromAmount" : int,
+    "to":"CURRENCY",
+    "toAmount" : int,
+    "address" : adressString,
+    "channel" : channelString
+  };
+
+  Can only push one bid at a time at the moment
+*/
 async function addBid(bid){
-  await globaldb.add(bid)
+  //bid to json
+  //var bidJSON = JSON.stringify(bid);
+  //await globaldb.add(bidJSON);
+  await globaldb.add(bid);
+  db = await orbitdb.feed(bid.channel);
+  channels.push(db); //create channel
+  await db.load();
+  await db.add(bid.address);
+}
+
+/*
+  Just send address and channel
+  jsonObject = {
+      "step" : "2",
+      "adress" : adressString,
+      "channel" : channelString
+    };
+*/
+async function acceptBid(bid){
+  db = await orbitdb.feed(bid.channel);
+  channels.push(db); //create channel
+  await db.load();
+  await db.add(bid.address);
+  //Also let everybody know that the bid is taken.
+}
+
+/*
+  Just send address and channel
+  jsonObject = {
+      "step" : "3",
+      "channel" : channelString,
+      "digest" : digestString,
+      "contractAddress" : contractAddressString
+    };
+*/
+async function pushContractInfo(contractInfo) {
+  //db has already been created, how to we access it?
+  //Need to add check to confirm that we have received the correct JsonObject
+  newJson = {
+    "step" : "3",
+    "digest" : contractInfo.digest,
+    "contractAddress" : contractInfo.contractAddress
+  };
+  await db.add(newJson);
+}
+
+/*
+  Just send address and channel
+  jsonObject = {
+      "step" : "4",
+      "channel" : channelString,
+      "contractAddress" : contractAddressString
+    };
+*/
+async function pushContractInfo(contractInfo) {
+  //db has already been created, how to we access it?
+  //Need to add check to confirm that we have received the correct JsonObject
+  newJson = {
+    "step" : "4",
+    "contractAddress" : contractInfo.contractAddress
+  };
+  await db.add(newJson);
+}
+
+
+/*
+What's left? Add functionality to take continiously take down the new information
+*/
+
+function processInfo(contractInfo) {
+  if(contractInfo.step == 1) {
+
+  } else if(contractInfo.step == 2) {
+
+  } else if(contractInfo.step == 3) {
+
+  } else if(contractInfo.step == 4) {
+
+  } else {
+
+  }
+
 }
 
 
