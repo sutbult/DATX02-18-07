9bed88e65d21b0683ff425660d56a83b5b7a5bb3
petrosdebesay <petrosd@student.chalmers.se>
2018-03-21 15:20:53 +0100

    Add messaging functionality

--- src/api/OrbitDBHandler.js
+++ src/api/OrbitDBHandler.js
@@ -1,6 +1,13 @@
 const Ipfs = require('ipfs')
 const OrbitDB = require('orbit-db')
 
+//1st: Bud och egen address.
+//2nd: accept bid, skickar sin egen adress.
+//1st: lägger upp ett kontrakt, skickar digest och konstraktsadress.
+//2nd: skickar konstraktsadress
+
+var channels = [];
+
 var ipfs = new Ipfs({
   repo: "ipfs/shared",
   config: {
@@ -17,7 +24,7 @@ var ipfs = new Ipfs({
 
 ipfs.once('ready', async function() {
 
-  var orbitdb = new OrbitDB(ipfs)
+  orbitdb = new OrbitDB(ipfs)
 
   const access = {
   // Give write access to everyone
@@ -25,19 +32,59 @@ ipfs.once('ready', async function() {
 }
   // init our database
    globaldb = await orbitdb.feed('/orbitdb/Qma6kDMVMeCxKjmnSa9xWSVYvYKq1Mwej2tr74gBrARjZ8/bids');
+   channels.push(globaldb);
 
   // load local cached db
   await globaldb.load();
 
 });
 
-async function addBid(bid){
-  await globaldb.add(bid)
+async function createChannel(channelName) {
+  return await orbitdb.feed(channelName);
 }
 
+/*
+Bid should be JSON in form of jsonObject = {
+    "from" : "CURRENCY",
+    "fromAmount" : int,
+    "to":"CURRENCY",
+    "toAmount" : int,
+    "address" : adressString,
+    "channel" : channelString
+  };
+
+  Can only push one bid at a time at the moment
+*/
+async function addBid(bid){
+  //bid to json
+  //var bidJSON = JSON.stringify(bid);
+  //await globaldb.add(bidJSON);
+  await globaldb.add(bid);
+  db = await orbitdb.feed(bid.channel);
+  channels.push(db); //create channel
+  await db.load();
+  await db.add(bid.address);
+}
+
+/*
+  Just send address and channel
+  jsonObject = {
+      "adress" : adressString,
+      "channel" : channelString
+    };
+*/
+async function acceptBid(bid){
+  db = await orbitdb.feed(bid.channel);
+  channels.push(db); //create channel
+  await db.load();
+  await db.add(bid.address);
+}
+
+
+
 function getBid(amount){
-  var bids = globaldb.iterator({ limit: 5 }).collect().map((e) => e.payload.value)
-  return JSON.stringify(bids, null, 2)
+  var bids = globaldb.iterator({ limit: 5 }).collect().map((e) => e.payload.value);
+  return JSON.stringify(bids, null, 2);
 }
 
 module.exports = {
