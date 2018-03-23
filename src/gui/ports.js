9acdc610f70745892c4ac48e809adda9f4096602
Samuel Utbult <samuel.utbult@hotmail.com>
2018-03-23 09:10:38 +0100

    Remove bad import
    Refs #24

--- src/gui/ports.js
+++ src/gui/ports.js
@@ -1,6 +1,5 @@
 "use strict"
 const Elm = require("./elm.js");
-const Api = require("../api/index.js");
 
 let container = document.getElementById("container");
 let app = Elm.Main.embed(container);
