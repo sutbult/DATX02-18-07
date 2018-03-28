99bce99a83a57ed6af86172b9c4894f6d638bc3c
Samuel Utbult <samuel.utbult@hotmail.com>
2018-03-28 08:47:38 +0200

    Implement Rest usage in Browse
    Refs #36

--- src/gui/elm/Browse/Rest.elm
+++ src/gui/elm/Browse/Rest.elm
@@ -1,7 +1,5 @@
 module Browse.Rest exposing (getBids)
 
-import Http
-
 import Browse.Types exposing (..)
 import Json.Decode exposing (..)
 
@@ -13,39 +11,17 @@ import Bid.Rest exposing
     ( decodeBid
     )
 
-import Error.Types
+import Rest
+
 
 getBids : Cmd Msg
 getBids =
-    let
-        request = Http.get "http://localhost:51337/api/getBids" decodeBids
-        onResponse result =
-            case Debug.log "Foo" result of
-                Ok bids ->
-                    SetBids bids
+    Rest.get
+        "getBids"
+        decodeBids
+        SetBids
+        Error
 
-                Err error ->
-                    Error <| Error.Types.Display "Connection error" <| errorMessage error
-    in
-        Http.send onResponse request
-
-errorMessage : Http.Error -> String
-errorMessage error =
-    case error of
-        Http.BadUrl url ->
-            "The given URL ''" ++ url ++ "'' is incorrect."
-
-        Http.NetworkError ->
-            "The client cannot connect to the blockchain server."
-
-        Http.Timeout ->
-            "The request timed out."
-
-        Http.BadStatus response ->
-            "The server responded with the status " ++ toString response.status.code ++ "."
-
-        Http.BadPayload descr _ ->
-            "The response is incorrectly formatted. See '" ++ descr ++ "'"
 
 decodeBids : Decoder (List Bid)
 decodeBids = list decodeBid
