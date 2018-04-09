68775bcfe3b7b0efa9eec50ce7d4f6e4818b7c77
Samuel Utbult <samuel.utbult@hotmail.com>
2018-04-09 19:20:52 +0200

    Add basic test

---
+++ packages/api/src/__tests__/index.js
@@ -0,0 +1,16 @@
+const api = require("../index.js");
+
+describe("Index", () => {
+    describe("get bids", () => {
+        it("can get bids", () => {
+            async function test() {
+                const bids = await api.getBids();
+                console.log("Dessa bud finns: ");
+                console.log(bids);
+                return true;
+            }
+            expect.assertions(1);
+            return expect(test()).resolves.toBe(true);
+        });
+    });
+});
