a2abcba05867b2160c83ccc1f25f3cb63a0e3b63
Jesper Persson <jesper.92.persson@gmail.com>
2018-04-17 22:50:37 +0200

    Add some more bitcoin functions and put loose code into functions to work in a flow

--- packages/api/src/bitcoin.js
+++ packages/api/src/bitcoin.js
@@ -7,6 +7,7 @@ var RpcClient = require('bitcoind-rpc');
 
 
 var rpc;
+var htlcTransactionObject;
 
 
 function BitcoinClient(user, password, host, port) {
@@ -43,20 +44,26 @@ function ping(callback) {
   });
 }
 
-function getBalance(callback) {
-  var result = {'error': '', 'balance': ''};
-  if (rpc === undefined) {
-    result.error = 'RPC needs to be defined';
-    callback(result);
-    return;
-  }
-  rpc.getBalance(function (err, ret) {
-    if (err) {
-      result.error = err;
-    } else {
-      result.balance = ret.result.toString();
+function getBalance() {
+  return new Promise((resolve, reject) => {
+    var result = {'error': '', 'balance': ''};
+    if (rpc === undefined) {
+      result.error = 'RPC needs to be defined';
+      resolve(result);
+      return;
     }
-    callback(result)
+    rpc.getBalance((err, ret) => {
+      if (err) {
+        reject(err);
+        //result.error = err;
+      } else {
+        resolve({
+          balance: ret.result.toString(),
+        });
+        //result.balance = ret.result.toString();
+      }
+      //resolve(result)
+    });
   });
 }
 
@@ -64,77 +71,142 @@ function myFun(word) {
   console.log(word);
 }
 
-BitcoinClient('bitcoinrpc', 'password', '127.0.0.1', '16592');
+BitcoinClient('bitcoinrpc', 'password', '127.0.0.2', '16592');
 // DefaultBitcoinClient();
-ping(myFun);
-/*
-var hashType = bitcoinjs.Transaction.SIGHASH_ALL
 
-var alice = bitcoinjs.ECPair.fromWIF('cRuoxDPY2ku1LjANJJMBUjqWYejYnF5MwmzbWsAs7i1uoVSqCmzH', bitcoinjs.networks.testnet); //Use your own key!
-var bob = bitcoinjs.ECPair.fromWIF('cRgLkMozC74fjazThBBQipMyk8yyL5z3NeDYe5pQ9ckhyo4Q7kCj', bitcoinjs.networks.testnet); //Use your own key!
+// async function functionName() {
+//   const something = await getBalance();
+//   return something;
+// }
+// functionName().then((a) => console.log(a));
+// console.log("hello");
+// ping(myFun
 
-var secret = "1"
+var timeout = getTimeout(110)
+result = htlcTransactionObject(undefined, undefined, undefined)
+console.log(result);
+var alice = bitcoinjs.ECPair.fromWIF('cRuoxDPY2ku1LjANJJMBUjqWYejYnF5MwmzbWsAs7i1uoVSqCmzH', bitcoinjs.networks.testnet);
+console.log(alice.getPublicKeyBuffer());
+
+redeemScript = result.redeemScript;
+hashType = result.hashType;
+
+function getTimeout(timeoutBlocks) {
+  //get current blockNumber first and add to timeoutBlocks
+  return bip65.encode({ blocks: timeoutBlocks });
+}
+
+function sendToHTLC(secret, sellerPublicKey, satoshis) {
+  this.htlcTransactionObject = htlcTransactionObject()
+}
+
+// function for sending to htlc address and adding id and stuff to htlcTransactionObject
+// htlcTransactionObject global object?
+// function for uploading and sending to htlc calls function above and htlcTransactionObject
+
+//
+
+
+
+function htlcTransactionObject(bobKey, secret, timeoutBlocks) {
+  var result = {};
+  var hashType = bitcoinjs.Transaction.SIGHASH_ALL;
+  result.hashType = hashType;
+
+  //dump private key for alice or get fromWIF some other way?
+  var alice = bitcoinjs.ECPair.fromWIF('cRuoxDPY2ku1LjANJJMBUjqWYejYnF5MwmzbWsAs7i1uoVSqCmzH', bitcoinjs.networks.testnet); //Use your own key!
+  //receive bobs fromWif as an arugment
+  var bob = bitcoinjs.ECPair.fromWIF('cRgLkMozC74fjazThBBQipMyk8yyL5z3NeDYe5pQ9ckhyo4Q7kCj', bitcoinjs.networks.testnet); //Use your own key!
+  var secret = "1"
+  // Get current block and add timeoutBlocks to that
+  var timeout = bip65.encode({ blocks: 110 }) //The block where you can refund the transaction after (in absolute value; check current block height)
+  result.timeout = timeout;
+  var redeemScript = htlc(bcrypto.sha256(secret), alice, bob, timeout);
+  result.redeemScript = redeemScript;
+  var scriptPubKey = bitcoinjs.script.scriptHash.output.encode(bitcoinjs.crypto.hash160(redeemScript));
+  var address = bitcoinjs.address.fromOutputScript(scriptPubKey, bitcoinjs.networks.testnet);
+  result.address = address;
+  console.log(address);
+  return result;
+}
 
 function toDigest(secret){
   return bcrypto.sha256(secret);
 }
 
-var timeout = bip65.encode({ blocks: 110 }) //The block where you can refund the transaction after (in absolute value; check current block height)
-
-function htlc (digest, seller, buyer, timeout) {
+function htlc (digest, sellerPublicKeyBuffer, buyerECPair, timeout) {
   return bitcoinjs.script.compile //CHECKSQUENCEVERIFY REFUSES TO WORK
   ([
     bitcoinjs.opcodes.OP_IF,
-      bitcoinjs.opcodes.OP_SHA256,
-      digest,
-      bitcoinjs.opcodes.OP_EQUALVERIFY,
-      bitcoinjs.opcodes.OP_DUP,
-      bitcoinjs.opcodes.OP_HASH160,
-      bcrypto.hash160(seller.getPublicKeyBuffer()),
+    bitcoinjs.opcodes.OP_SHA256,
+    digest,
+    bitcoinjs.opcodes.OP_EQUALVERIFY,
+    bitcoinjs.opcodes.OP_DUP,
+    bitcoinjs.opcodes.OP_HASH160,
+    bcrypto.hash160(sellerPublicKeyBuffer),
+    // bcrypto.hash160(seller.getPublicKeyBuffer()), we don't know the other person's private key...
     bitcoinjs.opcodes.OP_ELSE,
-      bitcoinjs.script.number.encode(timeout),
-      bitcoinjs.opcodes.OP_CHECKLOCKTIMEVERIFY,
-      bitcoinjs.opcodes.OP_DROP,
-      bitcoinjs.opcodes.OP_DUP,
-      bitcoinjs.opcodes.OP_HASH160,
-      bcrypto.hash160(buyer.getPublicKeyBuffer()),
+    bitcoinjs.script.number.encode(timeout),
+    bitcoinjs.opcodes.OP_CHECKLOCKTIMEVERIFY,
+    bitcoinjs.opcodes.OP_DROP,
+    bitcoinjs.opcodes.OP_DUP,
+    bitcoinjs.opcodes.OP_HASH160,
+    bcrypto.hash160(buyerECPair.getPublicKeyBuffer()),
     bitcoinjs.opcodes.OP_ENDIF,
     bitcoinjs.opcodes.OP_EQUALVERIFY,
     bitcoinjs.opcodes.OP_CHECKSIG
   ]);
 };
 
-
-var redeemScript = htlc(bcrypto.sha256(secret), alice, bob, timeout);
-
-var scriptPubKey = bitcoinjs.script.scriptHash.output.encode(bitcoinjs.crypto.hash160(redeemScript));
-var address = bitcoinjs.address.fromOutputScript(scriptPubKey, bitcoinjs.networks.testnet);
-
-console.log(address);
-
-var txb = new bitcoinjs.TransactionBuilder(bitcoinjs.networks.testnet);
-
-txb.setLockTime(timeout) //Transaction needs to have the appropriate locktime
-txb.addInput("0c09009f269f682d08cc53abae0b466409c55b95922e08d8a9edbef68de37fbf", 0, 0xfffffffe); //First argument is transaction ID of tx to P2SH address, second vout, third sequence
-txb.addOutput("2N6dyyk1a3L6keV5EENZe9jvf5NuXfjPuU2", 9e8); //First argument is destination for money and second is the amount
-
-var tx = txb.buildIncomplete();
-
-var signatureHash = tx.hashForSignature(0, redeemScript, hashType);
-var redeemScriptSig = bitcoinjs.script.scriptHash.input.encode([ //This whole thing is the stack that will run through the script
+function redeemAsSeller() {
+  var redeemScriptSig = bitcoinjs.script.scriptHash.input.encode([ //This whole thing is the stack that will run through the script
     bob.sign(signatureHash).toScriptSignature(hashType),
     bob.getPublicKeyBuffer(),
-    //Buffer.from("1"), //This is the secret
-    //bitcoinjs.opcodes.OP_TRUE
     bitcoinjs.opcodes.OP_FALSE
   ], redeemScript)
+  //call buildReedemTransaction
+}
 
-tx.setInputScript(0, redeemScriptSig);
+//
+function redeemAsBuyer(secret) {
+  var redeemScriptSig = bitcoinjs.script.scriptHash.input.encode([ //This whole thing is the stack that will run through the script
+    bob.sign(signatureHash).toScriptSignature(hashType),
+    bob.getPublicKeyBuffer(),
+    Buffer.from("1"), //This is the secret
+    bitcoinjs.opcodes.OP_TRUE
+  ], redeemScript)
+  //call buildReedemTransaction
+}
 
-console.log(bitcoinjs.script.decompile(redeemScript));
-console.log(bcrypto.hash160(alice.getPublicKeyBuffer()).toString('hex'));
+function buildReedemTransaction(htlcTransObj, htlcTransId, redeemScriptSig, network, destination, satoshis, vout, sequence) {
+  // var txb = new bitcoinjs.TransactionBuilder(bitcoinjs.networks.testnet);
+
+  // txb.setLockTime(timeout) //Transaction needs to have the appropriate locktime
+  // txb.addInput("0c09009f269f682d08cc53abae0b466409c55b95922e08d8a9edbef68de37fbf", 0, 0xfffffffe); //First argument is transaction ID of tx to P2SH address, second vout, third sequence
+  // txb.addOutput("2N6dyyk1a3L6keV5EENZe9jvf5NuXfjPuU2", 9e8); //First argument is destination for money and second is the amount
+  var txb = new bitcoinjs.TransactionBuilder(network);
+  txb.setLockTime(htlcTransObj.timeout);
+  txb.addInput(htlcTransId, 0, 0xfffffffe);
+  txb.addOutput(destination, satoshis);
+
+  var tx = txb.buildIncomplete();
+
+  var signatureHash = tx.hashForSignature(0, htlcTransObj.redeemScript, htlcTransObj.hashType);
+  // var redeemScriptSig = bitcoinjs.script.scriptHash.input.encode([ //This whole thing is the stack that will run through the script
+  //   bob.sign(signatureHash).toScriptSignature(hashType),
+  //   bob.getPublicKeyBuffer(),
+  //   //Buffer.from("1"), //This is the secret
+  //   //bitcoinjs.opcodes.OP_TRUE
+  //   bitcoinjs.opcodes.OP_FALSE
+  // ], redeemScript)
+
+  tx.setInputScript(0, redeemScriptSig);
+
+  console.log(bitcoinjs.script.decompile(redeemScript));
+  console.log(bcrypto.hash160(alice.getPublicKeyBuffer()).toString('hex'));
+
+}
 
 
 
-module.exports = {htlc, toDigest, alice, bob, tx, redeemScript};
-*/
+module.exports = {/*htlc, toDigest, alice, bob, tx, redeemScript*/};
