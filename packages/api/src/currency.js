eda38bf3040b63ef23c12e06cb61687002c59c04
Jacob Torrång <kopatheonlyone@hotmail.com>
2018-05-04 16:53:07 +0200

    Update order of functions in currency.js

--- packages/api/src/currency.js
+++ packages/api/src/currency.js
@@ -1,22 +1,35 @@
 function construct (balance, send, validate, claim, search, unlock, wallet) {
-    /** validate(contract, destination, value, digest, margin, timelock)
+    /** balance(address)
+     * @param {hex} address - The address to check the balance of
+     */
+    this.balance = balance;
+
+    /** send(account, digest, destination, value, refund_time)
+     * @param {hex} account - The address/account that sends and funds
+     * the contract
+     * @param {hex} digest - The digest that will lock the contract
+     * @param {hex} destination - The claim destination of the
+     * contract
+     * @param {int} value - The value to send to the contract
+     * expressed in the smallest unit
+     * @param {int} refund_time - How many (hours) the contract should
+     * be locked
+     */
+    this.send = send;
+
+    /** validate(contract, destination, value, digest, timelock, margin)
      * @param {hex} contract - The address of the contract
      * @param {hex} destination - The claim destination of the
      * contract
      * @param {int} value - The value in the contract expressed in the
      * smallest unit
      * @param {hex} digest - The digest that is locking the contract
-     * @param {int} margin - The acceptable amount of margin in
-     * timestamp difference
      * @param {int} timelock - The timestamp when the contract is
      * unlocked
+     * @param {int} margin - The acceptable amount of margin in
+     * timestamp difference
      */
     this.validate = validate;
-
-    /** balance(address)
-     * @param {hex} address - The address to check the balance of
-     */
-    this.balance = balance;
 
     /** claim(secret, account, contract)
      * @param {string} secret - The pre image hash of the digest in
@@ -44,18 +57,7 @@ function construct (balance, send, validate, claim, search, unlock, wallet) {
      */
     this.unlock = unlock;
 
-    /** send(account, digest, destination, value, refund_time)
-     * @param {hex} account - The address/account that sends and funds
-     * the contract
-     * @param {hex} digest - The digest that will lock the contract
-     * @param {hex} destination - The claim destination of the
-     * contract
-     * @param {int} value - The value to send to the contract
-     * expressed in the smallest unit
-     * @param {int} refund_time - How many (hours) the contract should
-     * be locked
-     */
-    this.send = send;
+
 
     /** wallet()
      */
