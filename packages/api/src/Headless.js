9fb5f704b731e7c08091fae2d4054cf04f0e7e44
KevinCT <kevinchentrieu@gmail.com>
2018-05-06 16:34:36 +0200

    Change to headless mode

--- packages/api/src/Headless.js
+++ packages/api/src/Headless.js
@@ -2,18 +2,16 @@ const HeadlessChrome = require("simple-headless-chrome")
 var path = require("path")
 
 var directory;
-var dbAddress;
 var browser;
 var browsers = []
 
 async function init() {
     directory = path.resolve("./");
-    dbAddress = "null";
 }
 async function createDB(name, type, permission) {
     try {
         browser = new HeadlessChrome({
-          headless: false // If you turn this off, you can actually see the browser navigate with your instructions
+          headless: true // If you turn this off, you can actually see the browser navigate with your instructions
           // see above if using remote interface
         });
         await browser.init()
@@ -34,16 +32,12 @@ async function createDB(name, type, permission) {
         }
 
         await mainTab.click("#createBtn")
-        await mainTab.wait(5000)
+        await mainTab.wait(9000)
         const address = await mainTab.evaluate(function(selector) {
           const selectorHtml = document.querySelector(selector)
           return selectorHtml.value
-      }, '#address');
-
-      dbAddress = address.result.value
-//        await mainTab.onConsole(listener)
-      //  await mainTab.wait(6000)
-        return dbAddress
+        }, "#address");
+        return address.result.value
     } catch (err) {
         console.log("ERROR!", err)
     }
@@ -63,13 +57,6 @@ async function closeAll(){
      browsers.splice(i,1);
   }
   await Promise.all(promises)
-}
-
-function listener(word) {
-    var string = JSON.stringify(word, null, 2)
-    if (string.includes("orbit")) {
-        dbAddress = word[0].value
-    }
 }
 
 module.exports = {
